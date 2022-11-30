937df063bf64699ebc84fc896cdfd6c4e20c8397
Jontzey <Jonathan.andersson1997@outlook.com>
2022-11-30 11:38:28 +0100

    More CRUD and Design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,8 +26,46 @@ namespace MoviePal
         {
             InitializeComponent();
 
+            using (AppDbContext context = new())
+            {
+                List<director> directors = context.Directors.ToList();
 
+                // POpulate directors
+                foreach (director director in directors)
+                {
+                    ComboBoxItem item = new ComboBoxItem();
 
+                    item.Content = $"{director.Firstname} {director.LastName}";
+                    item.Tag = director;
+                    cbDirectors.Items.Add(item);
+                }
+                //populate actors
+                List<actor> actors = context.Actors.ToList();
+
+                foreach (actor actor in actors)
+                {
+                    ListViewItem item = new ListViewItem();
+
+                    item.Content = $"{actor.FirstName} {actor.LastName}";
+                    item.Tag = actor;
+
+                    lvActors.Items.Add(item);
+                }
+
+                List<Movie> movies = context.Movies.ToList();
+
+                foreach(Movie movie in movies)
+                {
+                    ComboBoxItem item = new ComboBoxItem();
+
+                    item.Content = movie.Title;
+                    item.Tag= movie;
+
+                    cbMovies.Items.Add(item);
+
+                }
+            }
+                
         }
 
         private void btnDirector_Click(object sender, RoutedEventArgs e)
@@ -53,6 +91,28 @@ namespace MoviePal
                     FirstName = txtActorFirstName.Text,
                     LastName = txtActorLastName.Text,
                 });
+                context.SaveChanges();
+            }
+        }
+
+        private void btnAddMovie_Click(object sender, RoutedEventArgs e)
+        {
+            using (AppDbContext context = new())
+            {
+                Movie NewMovie = new Movie();
+
+                NewMovie.Title = txtMovieTitle.Text;
+                NewMovie.IsWatched = (bool)xbMovieWatched.IsChecked!;
+
+
+                ComboBoxItem selectedItem = cbDirectors.SelectedItem as ComboBoxItem;
+                director selectedDirector = selectedItem.Tag as director;
+
+                director dbDirector = context.Directors.FirstOrDefault(d => d.DirectorId == selectedDirector.DirectorId);
+
+                NewMovie.Director = dbDirector;
+
+                context.Movies.Add(NewMovie);
                 context.SaveChanges();
             }
         }
